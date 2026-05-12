7ffa866be8a6b423e12d65b8e28ed21c18d61bca
akPupa <akash@pupaclic.in>
2026-05-12 11:59:57 +0530

    Update CORS configuration to allow all origins

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
